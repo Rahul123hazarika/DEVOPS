713fbd7f54b098cfc6c5652c7e2687febf1fc57c
RAHUL HAZARIKA <98834787+Rahul123hazarika@users.noreply.github.com>
2026-05-21 14:09:31 +0530

    Update route.py

--- Flask/route.py
+++ Flask/route.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 @app.route('/'): #route
 def home():
   return 'welcome'
-@app.route('/second'): #route
+@app.route('api/<namw>'): #route
   return "hi !"
 if __name__=='__main__':
   app.run(debug=True)
