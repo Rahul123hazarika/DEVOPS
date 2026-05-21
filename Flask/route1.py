96bb0f24db996622a9b0cea9360281cc4a50d92f
RAHUL HAZARIKA <98834787+Rahul123hazarika@users.noreply.github.com>
2026-05-21 14:20:24 +0530

    Add Flask routes for home and API endpoint

---
+++ Flask/route1.py
@@ -0,0 +1,18 @@
+#url request, route 
+from flask import Flask,request
+app=Flask(__name__)
+@app.route('/')
+def home():
+  return "welcome to home page"
+@app.route('/api')
+def name():
+  name=request.values.get('name')
+  age=request.values.get('age')
+  age=int(age)
+  if age>=18:
+    return "welcome to the site' +name + '!'
+  else:
+    return "sorry , you are below 18"
+if __name__="__main__":
+   app.run(debug=True)
+
