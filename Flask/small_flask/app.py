e4de1107f55470318e9c389e04350657a4838ec2
RAHUL HAZARIKA <98834787+Rahul123hazarika@users.noreply.github.com>
2026-05-24 20:11:08 +0530

    Update Flask app to display current time and day
    Refactor Flask app to include current time and day of the week.

--- Flask/small_flask/app.py
+++ Flask/small_flask/app.py
@@ -1,21 +1,18 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
+from datetime import datetime 
 app=Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('index.html')
-@app.route('/api')
-def name():
-    name=request.values.get('name')
-    age=request.values.get('age')
+    day_of_week=datetime.now().strftime('%A') 
+    #.strftime('%A'): This is a method called "string format time." It takes the datetime object and formats it into a human-readable string based on the code you provide inside the parentheses.
+    # %A: This specific directive tells Python to return the full name of the weekday (e.g., Sunday).
+    current_time=datetime.now().strftime('%H:%M:%S')
+    return render_template('index.html',day_of_week=day_of_week,current_time=current_time)
 
-    age= int(age)
-    if age>18: 
-        return 'welcome to the site,'+name+'!'
-    else:
-        return 'sorry , you are too young to visit the site'
+@app.route('/time')
+def time():
+    current_time=datetime.now().strftime('%H:%M:%S') #here H: hour , M: minute , S: second
+    return current_time
 if __name__=='__main__':
-    app.run(debug=True) 
-
-    
-
-    
+    app.run(debug=True)
+     
